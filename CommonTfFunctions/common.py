68de78570f32be593663036c9bb7fc9f0f3200af
Olaf 3 <91374689+OFALOFAL@users.noreply.github.com>
2023-06-25 11:53:54 +0200

    Update common.py

--- CommonTfFunctions/common.py
+++ CommonTfFunctions/common.py
@@ -244,7 +244,7 @@ def create_tensorboard_callback(dir_name, experiment_name):
     dir_name: target directory to store TensorBoard log files
     experiment_name: name of experiment directory (e.g. efficientnet_model_1)
   """
-  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  log_dir = dir_name + "/" + experiment_name + "/" + datetime.now().strftime("%Y%m%d-%H%M%S")
   tensorboard_callback = tf.keras.callbacks.TensorBoard(
       log_dir=log_dir
   )
