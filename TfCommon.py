ea9990e7c569f1117478b4a05cc1641897b4a794
Olaf 3 <91374689+OFALOFAL@users.noreply.github.com>
2023-07-13 11:00:14 +0200

    Added load_and_prep_image func
    Reads in an image from filename, turns it into a tensor and reshapes into
    (224, 224, 3).
    
    Parameters
      ----------
      filename (str): string filename of target image
      img_shape (int): size to resize target image to, default 224
      scale (bool): whether to scale pixel values to range(0, 1), default True

--- TfCommon.py
+++ TfCommon.py
@@ -10,6 +10,29 @@ import zipfile
 from datetime import datetime
 from sklearn.metrics import confusion_matrix
 
+def load_and_prep_image(filename, img_shape=224, scale=True):
+  """
+  Reads in an image from filename, turns it into a tensor and reshapes into
+  (224, 224, 3).
+
+  Parameters
+  ----------
+  filename (str): string filename of target image
+  img_shape (int): size to resize target image to, default 224
+  scale (bool): whether to scale pixel values to range(0, 1), default True
+  """
+  # Read in the image
+  img = tf.io.read_file(filename)
+  # Decode it into a tensor
+  img = tf.io.decode_image(img)
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape])
+  if scale:
+    # Rescale the image (get all values between 0 and 1)
+    return img/255.
+  else:
+    return img
+
 def autolabel(rects): # From: https://matplotlib.org/examples/api/barchart_demo.html
   """
   Attach a text label above each bar displaying its height (it's value).
