2e772cd86bfdddccf8c1717227398c6a42de8c2b
Olaf 3 <91374689+OFALOFAL@users.noreply.github.com>
2023-07-20 12:18:04 +0200

    added calculate_results

--- TfCommon.py
+++ TfCommon.py
@@ -9,6 +9,28 @@ import numpy as np
 import zipfile
 from datetime import datetime
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+def calculate_results(y_true, y_pred):
+  """
+  Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+
+  Args:
+  -----
+  y_true = true labels in the form of a 1D array
+  y_pred = predicted labels in the form of a 1D array
+
+  Returns a dictionary of accuracy, precision, recall, f1-score.
+  """
+  # Calculate model accuracy
+  model_accuracy = accuracy_score(y_true, y_pred) * 100
+  # Calculate model precision, recall and f1 score using "weighted" average
+  model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+  model_results = {"accuracy": model_accuracy,
+                  "precision": model_precision,
+                  "recall": model_recall,
+                  "f1": model_f1}
+  return model_results
 
 def load_and_prep_image(filename, img_shape=224, scale=True):
   """
