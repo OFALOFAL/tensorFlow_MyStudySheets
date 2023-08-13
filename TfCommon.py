25ca18421ddfad238766ac368c9759c276232ac2
Olaf 3 <91374689+OFALOFAL@users.noreply.github.com>
2023-08-13 11:33:06 +0200

    Added plot_time_series func
    Plots a timesteps (a series of points in time) against values (a series of values across timesteps).

--- TfCommon.py
+++ TfCommon.py
@@ -12,6 +12,26 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 import time
 
+def plot_time_series(timesteps, values, format='.', start=0, end=None, label=None):
+  """
+  Plots a timesteps (a series of points in time) against values (a series of values across timesteps).
+  
+  Parameters
+  ---------
+  timesteps : array of timesteps
+  values : array of values across time
+  format : style of plot, default "."
+  start : where to start the plot (setting a value will index from start of timesteps & values)
+  end : where to end the plot (setting a value will index from end of timesteps & values)
+  label : label to show on plot of values
+  """
+  plt.plot(timesteps[start:end], values[start:end], format, label=label)
+  plt.xlabel("Time")
+  plt.ylabel("BTC Price")
+  if label:
+    plt.legend(fontsize=14)
+  plt.grid(True)
+
 def pred_timer(model, samples):
   """
   Times how long a model takes to make predictions on samples.
